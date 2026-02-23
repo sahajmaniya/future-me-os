1cb00d009c9150597428d4fbc24d24bb2450b6a4
Sahaj Maniya <112193004+sahajmaniya@users.noreply.github.com>
2026-02-23 11:14:28 -0800

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center text-white">
+      <h1 className="text-3xl">Paste the FutureMe OS App.jsx code here</h1>
+    </div>
+  )
+}
